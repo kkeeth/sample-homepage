4f8dd192d5cc511fae40c1815976fe2c966e2d90
kkeeth <zensin0082@gmail.com>
2024-01-26 02:05:56 +0900

    feat: footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
-import "./Footer.css";
-
 const Footer = () => {
   return (
     <>
-      <footer>
-        <p>© kkeeth 2024 -</p>
+      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <a href="https://x.com/kuwahara_jsri" className="hover:underline">
+              kkeeth
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
       </footer>
     </>
   );
