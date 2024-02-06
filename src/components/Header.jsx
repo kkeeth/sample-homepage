71841fe2588c5d6457e0ee8a465f8ae0a597521f
kkeeth <4125257+kkeeth@users.noreply.github.com>
2024-02-06 18:20:36 +0900

    styling: output page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
 
   return (
     <>
-      <header className="bg-white shadow sticky top-0 z-10">
+      <header className="w-dvw bg-white shadow sticky top-0 z-10">
         <nav
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
           aria-label="Global"
--- src/pages/Output.jsx
+++ src/pages/Output.jsx
@@ -7,21 +7,22 @@ import riotJPG from "../assets/riot_cover.jpg";
 const Output = () => {
   return (
     <>
-      <div className="flex-row md:flex md:flex-col justify-center items-center p-4 gap-8">
+      <div className="w-full flex-row md:flex md:flex-col justify-center items-center p-4 gap-8">
         <div className="w-full md:flex md:justify-between">
           <div className="md:w-1/2 p-4">
             <img
               src={micPNG}
               alt="ラジオのフリー画像"
-              className="w-full h-auto"
+              className="h-auto m-auto"
+              width="500"
             />
           </div>
-          <div className="md:w-1/2 p-4">
-            <h2 className="text-5xl font-semibold text-[#747bff] mb-8 underline">
+          <div className="flex flex-col justify-center md:w-1/2 p-4 max-md:text-center">
+            <h2 className="text-4xl font-semibold text-[#747bff] mb-8">
               ポッドキャスト
             </h2>
             <div className="grid grid-cols-2 gap-4 mt-4">
-              <div className="relative h-60 w-full rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+              <div className="relative md:h-60 w-full rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                 <a
                   href="https://art19.com/shows/kkeethengineers"
                   target="_blank"
@@ -30,14 +31,14 @@ const Output = () => {
                   <img
                     src="https://content.production.cdn.art19.com/images/c7/1f/e6/af/c71fe6af-3d1b-4566-9096-cdb13e39ced9/6f600fffb78b7af870a56fcf8dcb49757bbf5d368530275f60e49138d0ab91dbb2ed7ca9aeda45198ad73ac45bad2010c8297c2108fc17b00eea0c039505fb44.jpeg"
                     alt="kkeethのエンジニア雑談チャンネルのロゴ"
-                    className="h-full w-full object-cover object-center rounded-lg border-2"
+                    className="h-full w-auto object-cover object-center rounded-lg border-2 m-auto"
                   />
                   <p className="text-center mt-2 text-sm">
                     kkeethのエンジニア雑談チャンネル
                   </p>
                 </a>
               </div>
-              <div className="relative h-60 w-full rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+              <div className="relative md:h-60 w-full rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                 <a
                   href="https://stand.fm/channels/5e70dd5881d4e84e1ff1cab4"
                   target="_blank"
@@ -46,7 +47,7 @@ const Output = () => {
                   <img
                     src={standfmJPEG}
                     alt="Web業界のなんでも雑談室のロゴ"
-                    className="h-full w-full object-cover object-center rounded-lg border-2"
+                    className="h-full w-auto object-cover object-center rounded-lg border-2 m-auto"
                   />
                   <p className="text-center mt-2 text-sm">
                     Web業界のなんでも雑談室
@@ -57,12 +58,12 @@ const Output = () => {
           </div>
         </div>
         <div className="w-full md:flex md:justify-between">
-          <div className="md:w-1/2 py-12 px-24">
-            <h2 className="text-5xl font-semibold text-[#747bff] mb-8 underline">
+          <div className="md:w-1/2 py-12 px-24 lg:ml-20 max-md:text-center">
+            <h2 className="text-5xl font-semibold text-[#747bff] mb-8">
               npm
             </h2>
-            <ul className="list-outside text-xl">
-              <li className="list-disc ">
+            <ul className="list-inside text-2xl underline list-disc max-md:list-none">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/npm-stats-api"
                   target="_blank"
@@ -72,7 +73,7 @@ const Output = () => {
                   npm-stats-api
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/ja-greetings"
                   target="_blank"
@@ -82,7 +83,7 @@ const Output = () => {
                   ja-greetings
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/check-stats-modules"
                   target="_blank"
@@ -92,7 +93,7 @@ const Output = () => {
                   check-stats-modules
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/get-connpass-items"
                   target="_blank"
@@ -102,7 +103,7 @@ const Output = () => {
                   get-connpass-items
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/@kkeeth/card"
                   target="_blank"
@@ -112,7 +113,7 @@ const Output = () => {
                   @kkeeth/card
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/cmd-ranking"
                   target="_blank"
@@ -122,7 +123,7 @@ const Output = () => {
                   cmd-ranking
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/riot-barcode"
                   target="_blank"
@@ -132,7 +133,7 @@ const Output = () => {
                   riot-barcode
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/wcmd"
                   target="_blank"
@@ -142,7 +143,7 @@ const Output = () => {
                   wcmd
                 </a>
               </li>
-              <li className="list-disc">
+              <li className="">
                 <a
                   href="https://www.npmjs.com/package/riotx"
                   target="_blank"
@@ -155,7 +156,7 @@ const Output = () => {
             </ul>
           </div>
           <div className="md:w-1/2 p-4 ">
-            <img src={npmSVG} alt="画像2" className="w-full h-auto" />
+            <img src={npmSVG} alt="画像2" className="h-auto m-auto" width="500" />
           </div>
         </div>
         <div className="w-full md:flex md:justify-between">
@@ -163,11 +164,12 @@ const Output = () => {
             <img
               src={riotJPG}
               alt="拙著 riotjs の画像"
-              className="w-full h-auto"
+              className="h-auto m-auto"
+              width="500"
             />
           </div>
-          <div className="md:w-1/2 p-4">
-            <h2 className="text-5xl font-semibold text-[#747bff] mb-8 underline">
+          <div className="flex flex-col justify-center md:w-1/2 p-4 max-md:text-center">
+            <h2 className="text-5xl font-semibold text-[#747bff] mb-8">
               書籍
             </h2>
             <div>
@@ -175,9 +177,9 @@ const Output = () => {
                 href="https://amzn.asia/d/1JvuDvN"
                 target="_blank"
                 rel="noreferrer"
-                className="flex"
+                className="flex underline max-md:justify-center"
               >
-                <span className="text-xl">Riot.jsで簡単Webアプリ開発</span>
+                <span className="text-xl">Riot.jsで簡単Webアプリ開発 - C&R研究所出版</span>
                 <LinkIcon className="h-6 w-6 ml-2" />
               </a>
             </div>
